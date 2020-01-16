2dfd8560ddbc26a1f93cccd7bfabe7f23acebdac
Michi-Parz <michael.parzinger@web.de>
2020-01-16 21:40:24 +0100

    Muss noch auf alle Kombinationen erweitert werden.

--- solution_s4.R
+++ solution_s4.R
@@ -1,3 +1,8 @@
+# ToDo:
+# Fehlermeldungen anpassen: Beide Fehlermeldungen wenn zwei falsch, die Meldungen aus der Vorlage ect.
+# DRY überprüfen
+
+
 # Unterfuntionen---------------------------------------------------------------
 validity_subclass <- function(object, weight_area, hunt_area, hide = TRUE) {
   invalids <- character(0)
@@ -22,8 +27,7 @@ make_name <- function(length = 7) {
   name[seq(3, length, by = 2)] <-
     sample(consonants, size = ceiling(length / 2) - 1, replace = TRUE)
   name[seq(2, length, by = 2)] <-
-    sample(vowels, size = floor(length / 2), replace = TRUE
-    )
+    sample(vowels, size = floor(length / 2), replace = TRUE)
   paste(name, collapse = "")
 }
 
@@ -63,56 +67,74 @@ setClass("predator",
 
 
 
-# Unterklassen von prey---------------------------------------------------------
-setClass("mouse",
-  contains = "prey",
-  prototype = list(name = "mouse"),
-  validity = function(object) {
-    validity_subclass(object, weight_area = c(0.5, 1), hunt_area = c(0.6, 1))
-  }
+# Unterklassen von prey und predator--------------------------------------------
+
+set_classes <- function(object, ...) UseMethod("set_classes")
+set_classes.default <- function(name,
+                                contains = c("prey", "predator"),
+                                weight_area,
+                                hunt_area,
+                                prototype = NULL) {
+  checkmate::assert_character(name)
+  checkmate::assert_numeric(weight_area, min.len = 2, max.len = 2)
+  checkmate::assert_numeric(weight_area, min.len = 2, max.len = 2)
+  checkmate::assert_list(prototype, null.ok = TRUE)
+
+  contains <- match.arg(contains)
+
+  hide <- (contains == "prey")
+
+  setClass(name,
+    contains = contains,
+    prototype = prototype,
+    validity = function(object) {
+      validity_subclass(object,
+        weight_area = weight_area,
+        hunt_area = hunt_area,
+        hide = hide
+      )
+    }
+  )
+}
+
+set_classes.list <- function(list) {
+  set_classes.default(
+    name = list$name,
+    contains = list$contains,
+    weight_area = list$weight_area,
+    hunt_area = list$hunt_area
+  )
+}
+
+create_animals_list <- list(
+  # Preys
+  list(
+    "name" = "mouse", "contains" = "prey", "weight_area" = c(0.5, 1),
+    "hunt_area" = c(0.6, 1)
+  ),
+  list(
+    "name" = "rabbit", "contains" = "prey", "weight_area" = c(1, 5),
+    "hunt_area" = c(0.3, 0.8)
+  ),
+  list(
+    "name" = "deer", "contains" = "prey", "weight_area" = c(15, 30),
+    "hunt_area" = c(0.2, 0.7)
+  ),
+  # Predators
+  list(
+    "name" = "hawk", "contains" = "predator", "weight_area" = c(3, 8),
+    "hunt_area" = c(0.6, 1)
+  ),
+  list(
+    "name" = "lynx", "contains" = "predator", "weight_area" = c(20, 60),
+    "hunt_area" = c(0.5, 0.9)
+  )
 )
 
-setClass("rabbit",
-  contains = "prey",
-  prototype = list(name = "rabbit"),
-  validity = function(object) {
-    validity_subclass(object, weight_area = c(1, 5), hunt_area = c(0.3, 0.8))
-  }
-)
 
-setClass("deer",
-  contains = "prey",
-  prototype = list(name = "deer"),
-  validity = function(object) {
-    validity_subclass(object, weight_area = c(15, 30), hunt_area = c(0.2, 0.7))
-  }
-)
 
-# Unterklassen von predator-----------------------------------------------------
+lapply(create_animals_list, set_classes)
 
-setClass("hawk",
-  contains = "predator",
-  prototype = list(name = "hawk"),
-  validity = function(object) {
-    validity_subclass(object,
-      weight_area = c(3, 8),
-      hunt_area = c(0.6, 1),
-      hide = FALSE
-    )
-  }
-)
-
-setClass("lynx",
-  contains = "predator",
-  prototype = list(name = "hawk"),
-  validity = function(object) {
-    validity_subclass(object,
-      weight_area = c(20, 60),
-      hunt_area = c(0.5, 0.9),
-      hide = FALSE
-    )
-  }
-)
 
 
 # Methode-----------------------------------------
@@ -121,69 +143,67 @@ setMethod("show", "prey", function(object) {
   gender <- ifelse(object@female, "(f)", "(m)")
 
   cat(is(object)[[1]], " '", object@name, "' ", gender, "\n",
-    "  Weight: ", object@weight, "\n",
-    "  Hide:  ", object@hide, "\n",
+    "  weight: ", object@weight, "\n",
+    "  hide:  ", object@hide, "\n",
     sep = ""
   )
 })
 
 setMethod("show", "predator", function(object) {
   gender <- ifelse(object@female, "(f)", "(m)")
-  
+
   cat(is(object)[[1]], " '", object@name, "' ", gender, "\n",
-      "  Weight: ", object@weight, "\n",
-      "  Hide:  ", object@seek, "\n",
-      sep = ""
+    "  weight: ", object@weight, "\n",
+    "  seek:  ", object@seek, "\n",
+    sep = ""
   )
 })
 
 # Konstruktorfunktionen prey-------------------------------------------------
 
-mouse <- function(
-                  name = make_name(),
+# geht bestimmt schöner (DRY)
+
+
+mouse <- function(name = make_name(),
                   weight = runif(1, 0.5, 1),
                   hide = runif(1, 0.6, 1),
                   female = sample(c(TRUE, FALSE), 1)) {
   new("mouse", name = name, weight = weight, hide = hide, female = female)
 }
 
-rabbit <- function(
-  name = make_name(),
-  weight = runif(1, 1, 5),
-  hide = runif(1, 0.3, 0.8),
-  female = sample(c(TRUE, FALSE), 1)) {
+rabbit <- function(name = make_name(),
+                   weight = runif(1, 1, 5),
+                   hide = runif(1, 0.3, 0.8),
+                   female = sample(c(TRUE, FALSE), 1)) {
   new("rabbit", name = name, weight = weight, hide = hide, female = female)
 }
 
-deer <- function(
-  name = make_name(),
-  weight = runif(1, 15, 30),
-  hide = runif(1, 0.2, 0.7),
-  female = sample(c(TRUE, FALSE), 1)) {
+deer <- function(name = make_name(),
+                 weight = runif(1, 15, 30),
+                 hide = runif(1, 0.2, 0.7),
+                 female = sample(c(TRUE, FALSE), 1)) {
   new("deer", name = name, weight = weight, hide = hide, female = female)
 }
 
 # Konstruktorfunktionen predator-------------------------------------------------
 
-hawk <- function(
-  name = make_name(),
-  weight = runif(1, 3, 8),
-  seek = runif(1, 0.6, 1),
-  female = sample(c(TRUE, FALSE), 1)) {
+hawk <- function(name = make_name(),
+                 weight = runif(1, 3, 8),
+                 seek = runif(1, 0.6, 1),
+                 female = sample(c(TRUE, FALSE), 1)) {
   new("hawk", name = name, weight = weight, seek = seek, female = female)
 }
 
-lynx <- function(
-  name = make_name(),
-  weight = runif(1, 20, 60),
-  seek = runif(1, 0.5, 0.9),
-  female = sample(c(TRUE, FALSE), 1)) {
+lynx <- function(name = make_name(),
+                 weight = runif(1, 20, 60),
+                 seek = runif(1, 0.5, 0.9),
+                 female = sample(c(TRUE, FALSE), 1)) {
   new("lynx", name = name, weight = weight, seek = seek, female = female)
 }
 
 
 
-# Test-------------------------------------------------------------------------
+# Method test-------------------------------------------------------------------
 deer()
 hawk()
 str(mouse(female = TRUE))
@@ -191,3 +211,41 @@ str(hawk(weight = 4))
 str(lynx(name = "", weight = NA + 1))
 str(mouse(weight = 100))
 
+
+
+
+# Meet-----
+
+setGeneric(
+  "meet",
+  function(animal1, animal2,...) {
+    standardGeneric("meet")
+  }
+)
+
+
+setMethod("meet",
+  signature = c(animal1 = "animal", animal2 = "animal"),
+  function(animal1, animal2,text = "242") {
+    message(
+      is(animal1)[[1]], " '", animal1@name, "' & ", is(animal2)[[1]],
+      " '", animal2@name, "' ", text
+    )
+    
+  }
+)
+
+setMethod("meet",
+  signature = c(animal1 = "prey", animal2 = "prey"),
+  function(animal1, animal2) {
+    event <- c("ignore each other", "sniff each others' butts", 
+               "make sweet, sweet love")
+    text <- sample(event, size = 1, prob = c(1/4, 1/4, 1/2))
+    
+    callNextMethod(animal1,animal2, text)
+  }
+)
+
+# Andere Kombinationen und Geschlechter beachten und so
+
+meet(deer(), deer())
