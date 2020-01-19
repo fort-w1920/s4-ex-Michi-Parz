6980b4c043a65ff3bcc95d9023ad3913e8780f45
Michi-Parz <Michael.Parzinger@web.de>
2020-01-19 19:26:20 +0100

    Die Abwehr muss noch stabilisiert werden.

--- solution_s4.R
+++ solution_s4.R
@@ -4,19 +4,34 @@
 
 
 # Unterfuntionen---------------------------------------------------------------
-validity_subclass <- function(object, weight_area, hunt_area, hide = TRUE) {
+validity_subclass <- function(object, weight_area = c(0, Inf), hunt_area = NULL,
+                              hide = TRUE) {
+  
   invalids <- character(0)
-  check_weight <- (
-    object@weight >= weight_area[1] && object@weight <= weight_area[2]
-  )
-  check_hunt <- ifelse(hide,
-    (object@hide >= hunt_area[1] && object@hide <= hunt_area[2]),
-    (object@seek >= hunt_area[1] && object@seek <= hunt_area[2])
-  )
-  # Wenn beides falsch ist noch beides als Fehler ausgeben lassen!
-  if (!check_hunt) invalids <- "Fehlermeldung muss noch angepasst werden."
-  if (!check_weight) invalids <- "Unrealistic weight!"
-  if (length(invalids)) invalids else TRUE
+  invalids_index <- 1L
+  check_weight <- checkmate::test_numeric(object@weight, lower = weight_area[1],
+                                           upper = weight_area[2], any.missing = FALSE)
+  # check_weight <- (
+  #   object@weight >= weight_area[1] && object@weight <= weight_area[2] && 
+  #     !is.na(object@weight)
+  # )
+  
+  if (!is.null(hunt_area)) {
+    check_hunt <- ifelse(hide,
+                         (object@hide >= hunt_area[1] && object@hide <= hunt_area[2]),
+                         (object@seek >= hunt_area[1] && object@seek <= hunt_area[2])
+    )
+    if (!check_hunt) {
+      invalids[invalids_index] <- "hide/seek musst be in [0, 1]"
+      invalids_index <- invalids_index + 1L
+    } 
+  }
+  
+  if (!check_weight) {
+    invalids[invalids_index] <- paste("Weight musst be in [", weight_area[1], 
+                      ", ", weight_area[2], "]", sep = "")
+  } 
+  if (length(invalids) > 0) invalids else TRUE
 }
 
 make_name <- function(length = 7) {
@@ -38,7 +53,7 @@ setClass("animal",
   validity = function(object) {
     invalids <- character(0)
     no_name <- nchar(object@name) == 0
-    if (no_name) invalids <- "No <name> provided."
+    if (no_name) invalids <- "animals need a 'name'."
     if (length(invalids)) invalids else TRUE
   }
 )
@@ -142,30 +157,35 @@ lapply(create_animals_list, set_classes)
 # Nicht DRY genug!
 # Die Funktion außerhalb definieren und unterschiede übergeben!
 
+
+show_illustration <- function(object, subclass = c("prey", "predator")){
+  
+  subclass <- match.arg(subclass)
+  gender <- ifelse(object@female, "(f)", "(m)")
+  
+  hunt <- ifelse(subclass == "prey", object@hide, object@seek)
+  hunt_description <- ifelse(subclass == "prey", "  hide:  ", "  seek:  ")
+  
+  cat(is(object)[[1]], " '", object@name, "' ", gender, "\n",
+      "  weight: ", object@weight, "\n",
+      hunt_description, hunt, "\n",
+      sep = ""
+  )
+}
+
+
 setMethod("show", "prey", function(object) {
-  gender <- ifelse(object@female, "(f)", "(m)")
-
-  cat(is(object)[[1]], " '", object@name, "' ", gender, "\n",
-    "  weight: ", object@weight, "\n",
-    "  hide:  ", object@hide, "\n",
-    sep = ""
-  )
+    show_illustration(object, "prey")
 })
-
 setMethod("show", "predator", function(object) {
-  gender <- ifelse(object@female, "(f)", "(m)")
-
-  cat(is(object)[[1]], " '", object@name, "' ", gender, "\n",
-    "  weight: ", object@weight, "\n",
-    "  seek:  ", object@seek, "\n",
-    sep = ""
-  )
+  show_illustration(object, "predator")
 })
+  
+
+
+
 
 # Konstruktorfunktionen prey-------------------------------------------------
-
-# geht bestimmt schöner (DRY)
-
 
 mouse <- function(name = make_name(),
                   weight = runif(1, 0.5, 1),
@@ -217,8 +237,143 @@ str(mouse(weight = 100))
 
 
 
-# Meet--------------------------------------------------------------------------
+# Help functions for meet-------------------------------------------------------
 
+check_prey_or_predator <- function(animal) {
+  is_animal <- is(animal)
+  if (length(is_animal) != 3) {
+    stop("A subclass of prey or predator is necessary!")
+  }
+
+  is_animal[2]
+}
+
+
+
+prey_meets_predator <- function(prey, predator) {
+  probabilites <- c(
+    "ignore" = 1 / 2, "sniff" = 1 / 2, "love" = 0,
+    "fight" = 0, "kill" = 0, "escape" = 0
+  )
+  if (prey@weight >= 0.05 * predator@weight &
+    prey@weight <= 0.70 * predator@weight) {
+    prob_kill <- min(1, max(0, 0.6 + predator@seek - prey@hide))
+
+    probabilites <- c(
+      "ignore" = 0, "sniff" = 0, "love" = 0,
+      "fight" = 0, "kill" = prob_kill, "escape" = 1 - prob_kill
+    )
+  }
+
+  probabilites
+}
+
+
+prey_meets_prey <- function(prey1, prey2) {
+  same_species <- is(prey1)[1] == is(prey2)[1]
+  if (prey1@female != prey2@female & same_species) {
+    probabilites <- c(
+      "ignore" = 1 / 4, "sniff" = 1 / 4, "love" = 1 / 2,
+      "fight" = 0, "kill" = 0, "escape" = 0
+    )
+
+    return(probabilites)
+  }
+  probabilites <- c(
+    "ignore" = 1 / 2, "sniff" = 1 / 2, "love" = 0,
+    "fight" = 0, "kill" = 0, "escape" = 0
+  )
+
+  probabilites
+}
+
+
+
+predator_meets_predator <- function(predator1, predator2) {
+  same_species <- is(predator1)[1] == is(predator2)[1]
+  if (predator1@female != predator2@female & same_species) {
+    probabilites <- c(
+      "ignore" = 0, "sniff" = 0, "love" = 1 / 2,
+      "fight" = 1 / 2, "kill" = 0, "escape" = 0
+    )
+
+    return(probabilites)
+  }
+  probabilites <- c(
+    "ignore" = 1 / 2, "sniff" = 1 / 2, "love" = 0,
+    "fight" = 0, "kill" = 0, "escape" = 0
+  )
+
+  probabilites
+}
+
+
+sitaution_probability <- function(animal1, animal2) {
+  subclass_animal1 <- check_prey_or_predator(animal1)
+  subclass_animal2 <- check_prey_or_predator(animal2)
+
+  if (subclass_animal1 != subclass_animal2) {
+    if (subclass_animal1 == "prey") {
+      return(prey_meets_predator(prey = animal1, predator = animal2))
+    }
+
+    return(prey_meets_predator(predator = animal1, prey = animal2))
+  }
+
+  if (subclass_animal1 == "prey") {
+    return(prey_meets_prey(animal1, animal2))
+  }
+
+  predator_meets_predator(animal1, animal2)
+}
+
+
+hunt_message <- function(event, animal1, animal2) {
+  if (is(animal1)[2] == "prey") {
+    return(hunt_message(event, animal1 = animal2, animal2 = animal1))
+  }
+
+  if (event == "kill") {
+    return(message(
+      is(animal1)[[1]], " '", animal1@name, "' kills and eats ",
+      is(animal2)[[1]], " '", animal2@name, "'"
+    ))
+  }
+
+  message(
+    is(animal2)[[1]], " '", animal2@name, "'escapes from ",
+    is(animal1)[[1]], " '", animal1@name, "'"
+  )
+}
+
+
+
+situation <- function(animal1, animal2, probabilites) {
+  event <- sample(names(probabilites), size = 1, prob = probabilites)
+
+
+  if (event == "kill" | event == "escape") {
+    return(hunt_message(event, animal1, animal2))
+  }
+
+
+  intro <- paste(is(animal1)[[1]], " '", animal1@name, "' & ", is(animal2)[[1]],
+    " '", animal2@name, "'",
+    sep = ""
+  )
+
+  description <- switch(event,
+    "ignore" = "ignore each other",
+    "sniff" = "sniff each others' butts",
+    "love" = "make sweet, sweet love",
+    "fight" = "fight for territory"
+  )
+  message(intro, description)
+}
+
+
+
+# Meet Method-------------------------------------------------------------------
 setGeneric(
   "meet",
   function(animal1, animal2, ...) {
@@ -229,120 +384,42 @@ setGeneric(
 
 setMethod("meet",
   signature = c(animal1 = "animal", animal2 = "animal"),
-  function(animal1, animal2, text = "") {
-    message(
-      is(animal1)[[1]], " '", animal1@name, "' & ", is(animal2)[[1]],
-      " '", animal2@name, "' ", message
+
+  function(animal1, animal2, probability) {
+    situation(animal1, animal2, probability)
+  }
+)
+
+
+for (subclass1 in c("prey", "predator")) {
+  for (subclass2 in c("prey", "predator")) {
+    setMethod("meet",
+      signature = c(animal1 = subclass1, animal2 = subclass2),
+      function(animal1, animal2) {
+        probability <- sitaution_probability(animal1, animal2)
+
+        callNextMethod(animal1, animal2, probability)
+      }
     )
   }
+}
+
+# example code for animal class:
+set.seed(2020)
+animals <- list(
+  mouse(female = TRUE),
+  rabbit(),
+  hawk(female = TRUE),
+  deer(),
+  lynx(female = TRUE),
+  lynx(female = FALSE),
+  deer(),
+  mouse(female = FALSE),
+  deer(female = TRUE)
 )
 
-
-meet_list <- list(
-  list(
-    "contains1" = "prey", "contains2" = "prey",
-    "events" = c(
-      "ignore each other", "sniff each others' butts",
-      "make sweet, sweet love"
-    ),
-    "prob_same_sex" = c(1 / 2, 1 / 2, 0),
-    "prob_different_sex" = c(1 / 4, 1 / 4, 1 / 2)
-  ),
-  list(
-    "contains1" = "predator", "contains2" = "predator",
-    "events" = c(
-      "ignore each other", "sniff each others' butts",
-      "make sweet, sweet love", "fight for territory"
-    ),
-    "prob_same_sex" = c(1 / 3, 1 / 3, 1 / 3, 0),
-    "prob_different_sex" = c(0, 0, 1 / 2, 1 / 2)
-  ),
-  list(
-    "contains1" = "prey", "contains2" = "predator",
-    "events" = c(
-      "kills and eats", "escapes from",
-      "ignore each other", "sniff each others' butts"
-    ),
-    "prob_same_sex" = NULL,
-    "prob_different_sex" = NULL
-  )
-)
-
-# if if if if if if, wegbekommen
-
-
-create_situation <- function(animal1, animal2, contains1, contains2, 
-                                    events, prob_same_sex, prob_different_sex) {
-  probabilites <- prob_same_sex
-  if (animal1@female != animal2@female & is(animal1)[[1]] == is(animal2)[[1]]) {
-    probabilites <- prob_different_sex
+for (animal1 in animals[1:5]) {
+  for (animal2 in animals[9:5]) {
+    cat(meet(animal1, animal2))
   }
-  
-  if (contains1 != contains2) {
-    
-    probabilites <- c(0, 0, 1/2, 1/2)
-    
-    if (prey@weight >= 0.05 * predator@weight &
-      prey@weight <= 0.70 * predator@weight) {
-      prob_eat <- min(1, max(0,0.6 + predator@seek - prey@hide))
-      
-      probabilites <- c(prob_eat, 1 - prob_eat, 0, 0)
-    }
-    
-  }
-  
-  sample(events, size = 1, prob = probabilites)
 }
-
-
-
-
-
-for (i in seq_len(length(meet_list))) {
-
-}
-
-set_method_meet <- function(object, ...) UseMethod("set_classes")
-set_method_meet.default <- function(contains1, contains2, events,
-                            prob_same_sex, prob_different_sex) {
-  setMethod("meet",
-    signature = c(animal1 = contains1, animal2 = contains2),
-    function(animal1, animal2) {
-      message <- create_situation(
-        animal1, animal2, contains1, contains2, events,
-        prob_same_sex, prob_different_sex
-      )
-
-      callNextMethod(animal1, animal2, message)
-    }
-  )
-}
-
-set_method_meet.list <- function(list){
-  set_method_meet.default(list$contains1, list$contains2, list$events, 
-                          list$prob_same_sex, list$prob_different_sex)
-}
-
-
-setMethod("meet",
-  signature = c(animal1 = "prey", animal2 = "prey"),
-  function(animal1, animal2) {
-    event <- c(
-      "ignore each other", "sniff each others' butts",
-      "make sweet, sweet love"
-    )
-    
-    probabilites <- c(1 / 2, 1 / 2, 0)
-    if (animal1@female != animal2@female & is(animal1)[[1]] == is(animal2)[[1]]) {
-      probabilites <- c(1 / 4, 1 / 4, 1 / 2)
-    }
-
-    text <- sample(event, size = 1, prob = probabilites)
-
-    callNextMethod(animal1, animal2, text)
-  }
-)
-
-# Andere Kombinationen und Geschlechter beachten und so
-
-meet(deer(), deer())
